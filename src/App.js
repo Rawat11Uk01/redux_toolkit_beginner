94fc7c33a10b4a1cf33351ebe8e937e8a2493b73
alind <kashishrawat308@gmail.com>
2022-11-03 06:04:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect, useState } from "react";
 function App() {
+  const [state, setState] = useState([]);
+  const tempArr = [{name:'a', id:1}, {name:'b', id:2}, {name:'c', id:3}, {name:'d', id:4}, {name:'e', id:5}];
+  const addHandler = (e,a) => {
+    setState((prevState)=> [...prevState,a]);
+  };
+
+  console.log(state, "state");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {tempArr.map((a,i) => (
+        <button key={i} onClick={(e) => addHandler(e, a)}>{a.name}</button>
+      ))}
     </div>
   );
 }
